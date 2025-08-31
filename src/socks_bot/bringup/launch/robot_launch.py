499599f7a04bc8877eb65efd443c19c96e1f27f8
Mikhail Peshkov <mikhailpeshkov03@gmail.com>
2025-08-31 23:08:25 +0500

    gazebo simulation in progress

--- src/socks_bot/bringup/launch/robot_launch.py
+++ src/socks_bot/bringup/launch/robot_launch.py
@@ -4,7 +4,7 @@ from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitut
 import launch_ros
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, SetEnvironmentVariable
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
 import os
@@ -12,7 +12,82 @@ import os
 
 def generate_launch_description():
     pkg_share = get_package_share_directory("socks_bot")
-    default_model_path = os.path.join(pkg_share, 'description/socks_bot.urdf')
+    default_model_path = os.path.join(pkg_share, 'description/bot.xacro')
+    print(os.path.join(pkg_share, 'description/meshes'))
+    nodes = [
+        SetEnvironmentVariable(
+                'GZ_SIM_RESOURCE_PATH',
+                os.path.join(pkg_share, 'description/meshes')
+        ),
+        launch.actions.DeclareLaunchArgument(name='model', default_value=default_model_path,
+                                            description='Absolute path to robot urdf file'),
+        launch.actions.DeclareLaunchArgument(name='use_sim', default_value='True',
+                                            description='Flag to enable use_sim'),
+        launch.actions.DeclareLaunchArgument(name="micro_ros_serial_port", default_value="/dev/ttyAMA4",
+                                            description='esp32 serial port'),
+        launch.actions.DeclareLaunchArgument(name="lidar_serial_port", default_value="/dev/ttyAMA5",
+                                            description='lidar serial port'),
+    ]
+
+    
+
+    use_sim = LaunchConfiguration('use_sim')
+    
+
+    if not bool(use_sim):
+        hardware_plugin = "socks_bot/SocksBotHardware"
+        nodes.extend([
+            Node(
+                package='micro_ros_agent',
+                executable='micro_ros_agent',
+                name='micro_ros_agent',
+                arguments=["serial", "--dev", LaunchConfiguration('micro_ros_serial_port'), "-v6"]
+            ),
+            Node(
+                package='lds01rr',
+                executable='lds01rr',
+                parameters=[{"uart_port": LaunchConfiguration('lidar_serial_port')}]
+            ),
+            Node(
+                package='board_rebooter',
+                executable="board_rebooter",
+            ),
+        ])
+    else:
+        hardware_plugin = "gz_ros2_control/GazeboSystem"
+        pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim')
+        gz_launch_path = PathJoinSubstitution([pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py'])
+
+        nodes.extend([
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(gz_launch_path),
+                launch_arguments={
+                    'gz_args': ["empty.sdf"],  # Replace with your own world file
+                    'on_exit_shutdown': 'True'
+                }.items(),
+            ),
+            Node(
+                package='ros_gz_sim',
+                executable='create',
+                arguments=[
+                    '-name', 'socks_bot',            # имя модели в симуляции
+                    '-topic', 'robot_description', # откуда URDF брать
+                    '-z', '0.1'                    # высота над землёй
+                ],
+                output='screen'
+            ),
+
+            # Bridging and remapping Gazebo topics to ROS 2 (replace with your own topics)
+            # Node(
+            #     package='ros_gz_bridge',
+            #     executable='parameter_bridge',
+            #     arguments=['/example_imu_topic@sensor_msgs/msg/Imu@gz.msgs.IMU',],
+            #     remappings=[('/example_imu_topic',
+            #                 '/remapped_imu_topic'),],
+            #     output='screen'
+            # )
+        ])
+
 
     robot_controllers = PathJoinSubstitution(
         [
@@ -22,12 +97,14 @@ def generate_launch_description():
         ]
     )
 
+    robot_description = Command(['xacro ', LaunchConfiguration('model'), ' hardware_plugin:=', hardware_plugin])
+
     # Узел ros2_control_node с URDF и контроллерами
     control_node = Node(
         package="controller_manager",
         executable="ros2_control_node",
         parameters=[
-            {'robot_description': Command(['xacro ', LaunchConfiguration('model')])},
+            {'robot_description': robot_description},
             robot_controllers,
         ],
         output="both",
@@ -38,8 +115,8 @@ def generate_launch_description():
         package='robot_state_publisher',
         executable='robot_state_publisher',
         parameters=[
-            {'robot_description': Command(['xacro ', LaunchConfiguration('model')])},
-            {'use_sim_time': LaunchConfiguration('use_sim_time')}
+            {'robot_description': robot_description},
+            {'use_sim_time': use_sim}
         ]
     )
 
@@ -70,41 +147,13 @@ def generate_launch_description():
         launch_arguments={'params_file': os.path.join(pkg_share, 'config/slam_config.yaml')}.items()
     )
 
-    return LaunchDescription([
-        launch.actions.DeclareLaunchArgument(name='model', default_value=default_model_path,
-                                            description='Absolute path to robot urdf file'),
-        launch.actions.DeclareLaunchArgument(name='use_sim_time', default_value='False',
-                                            description='Flag to enable use_sim_time'),
-        launch.actions.DeclareLaunchArgument(name="micro_ros_serial_port", default_value="/dev/ttyAMA4",
-                                            description='esp32 serial port'),
-        launch.actions.DeclareLaunchArgument(name="lidar_serial_port", default_value="/dev/ttyAMA5",
-                                            description='lidar serial port'),
-
-        # Запуск micro-ROS агента
-        Node(
-            package='micro_ros_agent',
-            executable='micro_ros_agent',
-            name='micro_ros_agent',
-            arguments=["serial", "--dev", LaunchConfiguration('micro_ros_serial_port'), "-v6"]
-        ),
-
-        # Запуск лидара
-        Node(
-            package='lds01rr',
-            executable='lds01rr',
-            parameters=[{"uart_port": LaunchConfiguration('lidar_serial_port')}]
-        ),
-
-        # Перезагрузчик платы (если нужен)
-        Node(
-            package='board_rebooter',
-            executable="board_rebooter",
-        ),
-
+    nodes.extend([
         control_node,
         joint_state_broadcaster_spawner,
         robot_state_publisher_node,
         robot_controller_spawner,
         slam_launch,
     ])
+
+    return LaunchDescription(nodes)
 
